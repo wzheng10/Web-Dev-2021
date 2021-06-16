4e217b4eca5afe75414fb62f7d96d318aca23662
wzheng10 <wzheng100@gmail.com>
2021-06-16 13:19:44 -0700

    Refactoring delayedColorChange

--- 27 Async JS/creatingpromises/app.js
+++ 27 Async JS/creatingpromises/app.js
@@ -25,3 +25,38 @@ fakeRequest('/dogs/1')
     console.log('OH NO', err)
 })
 
+// OLD WAY
+// const delayedColorChange = (newColor, delay, doNext) => {
+//     setTimeout(() => {
+//         document.body.style.backgroundColor = newColor;
+//         doNext && doNext();
+//     }, delay)
+// }
+
+// delayedColorChange('red', 1000, () =>{
+//     delayedColorChange('orange', 1000, () => {
+//         delayedColorChange('yellow', 1000, () => {
+//             delayedColorChange('green', 1000, () => {
+        
+//             })
+//         })
+//     })
+// });
+
+//PROMISE METHOD
+const delayedColorChange = (color, delay) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            document.body.style.backgroundColor = color;
+            resolve();
+        }, delay)
+    })
+}
+
+delayedColorChange('red', 1000)
+.then(() => delayedColorChange('orange', 1000))
+.then(() => delayedColorChange('yellow', 1000))
+.then(() => delayedColorChange('green', 1000))
+.then(() => delayedColorChange('blue', 1000))
+.then(() => delayedColorChange('indigo', 1000))
+.then(() => delayedColorChange('violet', 1000))
