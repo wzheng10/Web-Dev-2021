b8201478bc3ed3045751c71db026ef17c3d49b30
wzheng10 <wzheng100@gmail.com>
2021-06-22 15:15:54 -0700

    More Complex EJS

--- 34_dynamicHTML_with_templating/template/index.js
+++ 34_dynamicHTML_with_templating/template/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const redditData = require('./data.json')
+const colors = require('colors')
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, '/views'))
@@ -10,9 +12,21 @@ app.get('/', (req, res) => {
   res.render('home')
 })
 
+app.get('/cats', (req, res) => {
+  const cats = [
+    'Blue', 'Rocket', 'Monty', 'Shadow', 'Maison'
+  ]
+  res.render('cats', {cats})
+})
+
 app.get('/r/:subreddit', (req, res) => {
   const {subreddit} = req.params;
-  res.render('subreddit', {subreddit})
+  const data = redditData[subreddit];
+  if(data) {
+    res.render('subreddit', {...data})
+  } else {
+    res.render('notfound', {subreddit})
+  }
 })
 
 app.get('/rand', (req, res) => {
