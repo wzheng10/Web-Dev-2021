282ec871cd4f13153c6db45b580fbf53338a517e
wzheng10 <wzheng100@gmail.com>
2021-06-29 14:33:44 -0700

    Create

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -17,17 +17,30 @@ db.once("open", () => {
 
 const app = express();
 
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.urlencoded({extended:true}))
 
 app.get('/campgrounds', async(req, res) => {
   const campgrounds = await Campground.find({});
   res.render('campgrounds/index', {campgrounds} )
 })
 
+app.get('/campgrounds/new', (req,res) => {
+  res.render('campgrounds/new')
+})
+
 app.get('/campgrounds/:id', async(req, res) => {
   const campground = await Campground.findById(req.params.id)
   res.render('campgrounds/show', {campground})
+})
+
+app.post('/campgrounds', async (req, res) => {
+  const campground = new Campground(req.body.campground);
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`)
 })
 // app.get('/makecampground', async (req, res) => {
 //   const camp = new Campground({title: 'My Backyard', description: 'cheap camping'});
