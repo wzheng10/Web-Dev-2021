aff79b80236ef726652ccf46f27e5fc3e833f054
wzheng10 <wzheng100@gmail.com>
2021-06-28 20:15:55 -0700

    Creating Products

--- 38_mongoose_with_express/index.js
+++ 38_mongoose_with_express/index.js
@@ -15,9 +15,29 @@ mongoose.connect('mongodb://localhost:27017/farmStand', {useNewUrlParser: true, 
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}))
 
-app.get('/dog', (req, res) => {
-  res.send('WOOF!')
+app.get('/products', async(req, res) => {
+  const products = await Product.find({})
+  res.render('products/index', {products})
+})
+
+app.get('/products/new', (req, res) => {
+  res.render('products/new')
+})
+
+app.post('/products', async (req, res) => {
+  const newProduct = new Product(req.body);
+  await newProduct.save();
+  console.log(newProduct)
+  res.redirect(`/products/${newProduct._id}`)
+})
+
+app.get('/products/:id', async (req, res) => {
+  const {id} = req.params;
+  const product = await Product.findById(id)
+  console.log(product);
+  res.render('products/show', {product})
 })
 
 app.listen(3000, () =>{
