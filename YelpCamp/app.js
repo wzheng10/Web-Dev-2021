ca13ec3747bb5f11d9cc61b70eacf448ac9117ff
wzheng10 <wzheng100@gmail.com>
2021-06-29 14:15:20 -0700

    Index and Show Page

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -20,15 +20,20 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
-app.get('/', (req, res) => {
-  res.render('home')
+app.get('/campgrounds', async(req, res) => {
+  const campgrounds = await Campground.find({});
+  res.render('campgrounds/index', {campgrounds} )
 })
 
-app.get('/makecampground', async (req, res) => {
-  const camp = new Campground({title: 'My Backyard', description: 'cheap camping'});
-  await camp.save();
-  res.send(camp)
+app.get('/campgrounds/:id', async(req, res) => {
+  const campground = await Campground.findById(req.params.id)
+  res.render('campgrounds/show', {campground})
 })
+// app.get('/makecampground', async (req, res) => {
+//   const camp = new Campground({title: 'My Backyard', description: 'cheap camping'});
+//   await camp.save();
+//   res.send(camp)
+// })
 
 
 app.listen(3000, ()=> {
