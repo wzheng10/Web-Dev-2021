6dbc97ca98bc2a3e5a8e1562d9d9a4569dcaf301
wzheng10 <wzheng100@gmail.com>
2021-07-02 09:01:09 -0700

    Authenticator for Specified  Routes

--- 40_middleware/middleware_intro/index.js
+++ 40_middleware/middleware_intro/index.js
@@ -13,13 +13,13 @@ app.use('/dogs', (req, res, next) => {
   console.log("I LOVE DOGS")
   next();
 })
-app.use((req, res, next) => {
+const  verifyPassword = (req, res, next) => {
   const {password} = req.query;
   if (password === 'chickennuggets'){
-    next();
+    return next();
   }
   res.send('SORRY, YOU NEED A PASSWORD!!!')
-})
+}
 // app.use((req, res, next) => {
 //   console.log("THIS IS MY FIRST MIDDLEWARE!!!!")
 //   return next();
@@ -38,12 +38,12 @@ app.get('/', (req, res) => {
   res.send('HOME PAGE!')
 })
 
-app.get('/dogs', (req, res) => {
+app.get('/dogs',verifyPassword, (req, res) => {
   console.log(`REQUEST DATE: ${req.requestTime}`)
   res.send('WOOF WOOF!')
 })
 
-app.get('/secret', (req, res) => {
+app.get('/secret', verifyPassword, (req, res) => {
   res.send('MY SECRET IS: I LOVE RUVEL')
 })
 app.use((req, res) => {
